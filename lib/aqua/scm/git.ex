8db80e53b3896a01a810e70958fcb52d4d012e52
Paul Fleischer <pf@qarmainspect.com>
2025-01-03 15:39:29 +0100

    Auto-detect branch-name instead of assuming "master"

--- lib/aqua/scm/git.ex
+++ lib/aqua/scm/git.ex
@@ -12,15 +12,24 @@ defmodule Aqua.SCM.Git do
   end
 
   def pull_repo(fs) do
-    case git_run(["-C", fs, "fetch", "origin", "master"]) do
+    branch_name = branch_name(fs)
+
+    case git_run(["-C", fs, "fetch", "origin", branch_name]) do
       {_message, 0} ->
-        case git_run(["-C", fs, "reset", "--hard", "origin/master"]) do
+        case git_run(["-C", fs, "reset", "--hard", "origin/#{branch_name}"]) do
           {message, 0} -> {:ok, message}
           error -> decorate_error(error)
         end
 
       error ->
         decorate_error(error)
+    end
+  end
+
+  defp branch_name(fs) do
+    case git_run(["-C", fs, "rev-parse", "--abbrev-ref", "HEAD"]) do
+      {branch_name, 0} -> String.trim(branch_name)
+      _ -> "master"
     end
   end
 
